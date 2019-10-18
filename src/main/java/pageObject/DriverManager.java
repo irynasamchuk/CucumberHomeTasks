4ba7b51f87b52cb7a0ad96caf8f8acd6f8e6745f
Iryna Samchuk <samchukicat@gmail.com>
2019-10-18 16:41:06 +0300

    add Chrome Options

--- src/main/java/pageObject/DriverManager.java
+++ src/main/java/pageObject/DriverManager.java
@@ -3,6 +3,7 @@ package pageObject;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverManager {
@@ -11,6 +12,8 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setBinary("C:\\Users\\Iryna_Samchuk\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
             driverWait = new WebDriverWait(driver, 15);
